54f2f77b7dd00bc79e2e25afc1a968e20666801d
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2024-09-23 05:50:18 +0900

    ✨ [Feat] #1 - 바텀시트 구현

--- SwiftUI_WeatherApp/SwiftUI_WeatherApp/SwiftUI_WeatherAppApp.swift
+++ SwiftUI_WeatherApp/SwiftUI_WeatherApp/SwiftUI_WeatherAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUI_WeatherAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CustomTabBarView()
         }
     }
 }
