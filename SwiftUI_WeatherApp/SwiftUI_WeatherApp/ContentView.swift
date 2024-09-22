08f7d720385c1cb408e21299eb6633c87fb3165e
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2024-09-23 01:00:54 +0900

    📝 [Setting] - 초기 파일 세팅

---
+++ SwiftUI_WeatherApp/SwiftUI_WeatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_WeatherApp
+//
+//  Created by 정민지 on 9/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_WeatherApp/SwiftUI_WeatherApp/SwiftUI_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherAppApp.swift
+//  SwiftUI_WeatherApp
+//
+//  Created by 정민지 on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
